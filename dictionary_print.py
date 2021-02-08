04bebdcecf72fde9801c78db2fca0ed25b3bfee5
hongyeongim6 <62275592+hongyeongim6@users.noreply.github.com>
2021-02-08 14:24:22 +0900

    Delete dictionary_print.py

--- dictionary_print.py
+++
@@ -1,7 +0,0 @@
-my_dict= { 'first name': 'Kim', 'last name' : 'Hong Yeon', 'age' : 26, 'address' : 'Seoul, Korea'} 
-my_dict['sex'] = 'Male'
-
-print(my_dict)
-
-for v,k in my_dict.items(): 
-  print('{!r:15s} : {}'.format(v, k))
